e389c150c20eea80e91c9ff75523aa93454e8275
Anders Nelson <andersnelson@Anderss-MacBook-Pro-15.local>
2021-05-20 09:40:37 -0500

    finishing up

--- js/people.js
+++ js/people.js
@@ -1,31 +1,43 @@
+/* 
+The main purpose of the javascript file is to display and hide some "details" about people.
+I just hardcoded an array of objects to symbolize what the data might look like.
+I set up a table to hold this additional data, and when the "view details" button is clicked,
+the display is set to block (instead of one), and the data is populated using the "document.getElementById" method.
+*/
 var people =[
     {
         "name": "Anders Nelson",
         "status": "Currently enrolled",
-        "classes": ["SENG 3230", "SENG 4310", "CWEB 1111"]
+        "classes": ["SENG 3230", "SENG 4310", "CWEB 1111"],
+        "phone": "111-111-1111",
+        "email": "anders@dunwoody.edu"
     },
     {
         "name": "Jane Doe",
-        "classes": ["CNTS 1200", "CWEB 3030"]
+        "classes": ["CNTS 1200", "CWEB 3030"],
+        "phone": "222-222-2222"
     },
     {
         "name": "Joe Student",
         "employer": "Target",
-        "phone": "555-555-5555"
+        "phone": "555-555-5555",
+        "email": "joe@target.com"
     }
 ]
+/* 
+This is the function to handle the display of the additional information.
+I used a bunch of if/else statements here to get the data to clear.  I was running
+into a problem where if I clicked between people and there was a field missing in the new person's data,
+the old data from the previous person would stay there, which I didn't want.
+*/
 function showPeople(index){
     // console.log(index)
-    var html = document.getElementById('people');
+    var html = document.getElementById('details');
 
 
     if (html.style.display === "none") {
         html.style.display = "block";
        } 
-    //else {
-    //     html.style.display = "none";
-    //   }
-    // document.getElementById('content').style.gridTemplateRows="200px 1000px 200px";
       document.getElementById('hidebutton').style.display='block';
     
     if (people[index].phone){
@@ -46,18 +58,18 @@ function showPeople(index){
     else{
         document.getElementById('currentClasses').innerHTML='';
     }
+    if (people[index].email){
+        document.getElementById('email').innerHTML=people[index].email;
+    }
+    else{
+        document.getElementById('email').innerHTML='';
+    }
     
 
 }
+// Another user defined function here, just to handle hiding the details view if the user wants to.
 function hideDetails(){
     document.getElementById('hidebutton').style.display='none';
-    document.getElementById('people').style.display='none';
+    document.getElementById('details').style.display='none';
     
 }
-// document.addEventListener("mouseenter", function(){
-//     var button = document.getElementsByClassName('button')
-
-// })
-// function buttonClick(){
-//     console.log('button click');
-// }
